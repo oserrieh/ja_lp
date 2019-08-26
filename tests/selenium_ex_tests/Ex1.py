598cf3a36eb3ddd3f29a41de017a71503f2caf02
omar <omar.serrieh@3pillarglobal.com>
2019-08-26 14:08:13 +0300

    Finished first ex.

---
+++ tests/selenium_ex_tests/Ex1.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get('http://www.google.com')
+
+q = driver.find_element(By.NAME, 'q')
+q.send_keys('selenium webdriver')
+driver.get('https://www.seleniumhq.org/projects/webdriver/')
+
+driver.quit()
+
