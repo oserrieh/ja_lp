f2a5dde23b58fb0b704ad8b4bb0523d5cbd0de7f
oserrieh <omar.serrieh@3pillarglobal.com>
2019-09-02 15:11:05 +0300

    Added comments and fixed ex.

--- tests/selenium_ex_tests/test_selenium_ex1.py
+++ tests/selenium_ex_tests/test_selenium_ex1.py
@@ -1,43 +1,45 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 
 driver = webdriver.Chrome()
 driver.get('http://www.google.com')
-
+driver.maximize_window()
+time.sleep(2)
 
 try:
-    driver.find_element_by_id('gbw')
-    driver.find_element_by_class_name('gb_f')
-    driver.find_element_by_class_name('hb2Smf')
-    driver.find_elements_by_partial_link_text('/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')
+    driver.find_element_by_class_name('gNO89b')   ##Google Search-box class name
+    driver.find_element_by_name('btnI')           ##Name of the I'm Feeling Lucky box
+    driver.find_elements_by_partial_link_text('/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')  ##link to google picture logo from google homepage
     print('Test passed: Elements found')
 except Exception as e:
     print('Exception found', format(e))
 
 
-q = driver.find_element(By.NAME, 'q')
-q.send_keys('selenium webdriver')
-q.submit()
+q = driver.find_element(By.NAME, 'q')       ## Find the searchbar from google.com
+q.send_keys('selenium webdriver')       ##Enter keys from keyboard
+time.sleep(1)
+q.submit()          ## Submit the google search
 
-
-first_one = driver.find_elements_by_link_text('https://www.seleniumhq.org/projects/webdriver/')[:1]
+first_one = driver.find_elements_by_link_text('https://www.seleniumhq.org/projects/webdriver/')[:1]     #Check's the first google search result
 for result in first_one:
     assert "selenium webriver" in result.text.lower(), "Result does not contain 'selenium webdriver'"
 
+driver.find_element_by_class_name('ellip').click()    ## Finding the class name for the first result in google searches(Selenium WebDriver) and click's it
+time.sleep(3)
 
-driver.get('https://www.seleniumhq.org/projects/webdriver/')
 
 try:
-    driver.find_elements_by_link_text('https://www.seleniumhq.org/projects/webdriver/')
-    driver.find_element_by_id('footerLogo')
-    driver.find_element_by_id('menu_download')
-    driver.find_element_by_class_name('downloadBox')
+    driver.find_elements_by_link_text('https://www.seleniumhq.org/projects/webdriver/')     ## Check's the link of the site
+    driver.find_element_by_id('footerLogo')         ## Check's the OpenGl Footer logo
+    driver.find_element_by_id('menu_download')      ##Check's the Download box menu from header
+    driver.find_element_by_class_name('downloadBox')        ## Check's the DownloadBox ---Download link for selenium--- in the right side menu up to donate section
     print('Test passed: Elements found')
 
 
 except Exception as e:
     print('Exception found', format(e))
 
-
+time.sleep(1.5)
 driver.quit()
