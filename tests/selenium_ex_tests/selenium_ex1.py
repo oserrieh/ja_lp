d0c571da890482348ac5728886d5d5faa3c7be82
oserrieh <omar.serrieh@3pillarglobal.com>
2019-09-16 11:11:46 +0300

    Added Expected Conditions

--- tests/selenium_ex_tests/selenium_ex1.py
+++ tests/selenium_ex_tests/selenium_ex1.py
@@ -1,8 +1,9 @@
 import unittest
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
 class SearchText(unittest.TestCase):
@@ -15,47 +16,44 @@ class SearchText(unittest.TestCase):
     # "Google Search" button class name
     search_box_button = 'gNO89b'
     # Google apps square dots near google account
-    square_dots = 'gb_Of'
-    # the footer logo from selenium webdriver site
+    square_dots = 'gb_C'
+    # the footer logo from selenium WebDiver site
     footer_logo = 'footerLogo'
+    # Class name of Selenium Webdriver's first result on google.
+    get_page = 'LC20lb'
     # Download box menu from header
     download_menu = 'menu_download'
     # DownloadBox ---Download link for selenium---
     # in the right side menu up to donate section
     download_box = 'downloadBox'
+    time = 5
 
     def setUp(self):
         # create a new Chrome session
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
-        self.driver.implicitly_wait(5)
+        self.wait = WebDriverWait(self.driver, self.time)
 
     def test_search_automation(self):
         # navigate to the google home page
         self.driver.get("http://www.google.com/")
-        self.assertTrue(self.is_element_present(By.NAME, self.search_field))
-        self.assertTrue(self.is_element_present(By.NAME, self.feeling_lucky))
-        self.assertTrue(self.is_element_present(By.CLASS_NAME, self.mic_search))
-        self.assertTrue(self.is_element_present(By.CLASS_NAME, self.search_box_button))
-        self.assertTrue(self.is_element_present(By.CLASS_NAME, self.square_dots))
+        self.wait.until(ec.visibility_of_element_located((By.NAME, self.search_field)))
+        self.wait.until(ec.presence_of_element_located((By.NAME, self.feeling_lucky)))
+        self.wait.until(ec.visibility_of_element_located((By.CLASS_NAME, self.mic_search)))
+        self.wait.until(ec.presence_of_element_located((By.CLASS_NAME, self.search_box_button)))
+        self.wait.until(ec.presence_of_element_located((By.CLASS_NAME, self.square_dots)))
         # get the search textbox, search for keywords and submit
-        self.driver.find_element_by_name(self.search_field).send_keys('Selenium WebDriver')
-        self.driver.find_element_by_name(self.search_field).submit()
+        search_for = self.wait.until(ec.element_to_be_clickable((By.NAME, self.search_field)))
+        search_for.send_keys('Selenium WebDriver')
+        search_for.submit()
         # First result on google search (the class name of Selenium webdriver)
-        self.driver.find_element_by_class_name('LC20lb').click()
-        self.assertTrue(self.is_element_present(By.ID, self.footer_logo))
-        self.assertTrue(self.is_element_present(By.ID, self.download_menu))
-        self.assertTrue(self.is_element_present(By.CLASS_NAME, self.download_box))
+        self.wait.until(ec.element_to_be_clickable((By.CLASS_NAME, self.get_page))).click()
+        self.wait.until(ec.presence_of_element_located((By.ID, self.footer_logo)))
+        self.wait.until(ec.presence_of_element_located((By.ID, self.download_menu)))
+        self.wait.until(ec.presence_of_element_located((By.CLASS_NAME, self.download_box)))
 
     def tearDown(self):
         self.driver.quit()  # close the browser window
-
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException:
-            return False
-        return True
 
 
 if __name__ == '__main__':
